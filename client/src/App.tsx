f35bbdce41907e9be9487d04c979568c89771b13
Hubert Yu <hubyu9234@gmail.com>
2025-05-20 15:57:26 -0700

    Fixed routing issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import ComingSoon from "./pages/ComingSoon";
 
