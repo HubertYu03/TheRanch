a22af2309fe0f1c28f66127ae92f4f2c6fcce8d3
Hubert Yu <hubyu9234@gmail.com>
2025-05-20 17:32:53 -0700

    Updated icon

--- client/src/pages/ComingSoon.tsx
+++ client/src/pages/ComingSoon.tsx
@@ -1,18 +1,26 @@
 import Logo from "@/components/Logo";
 import { motion } from "motion/react";
+import { useEffect } from "react";
 
 const duration: number = 0.3;
 
 const ComingSoon = () => {
+  useEffect(() => {
+    document.title = "The Ranch | Coming Soon";
+    return () => {
+      document.title = "The Ranch | Coming Soon";
+    };
+  }, []);
+
   return (
     <div
-      className="min-h-svh flex 
+      className="min-h-svh flex font-buenard
       flex-col items-center justify-center"
     >
       <Logo />
       <div className="flex flex-col items-center gap-5">
         <motion.div
-          className="text-6xl font-buenard sm:text-9xl"
+          className="text-6xl sm:text-9xl"
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: duration }}
@@ -21,7 +29,7 @@ const ComingSoon = () => {
         </motion.div>
         <motion.div
           className="text-xl text-gray-500 sm:text-3xl"
-          initial={{ opacity: 0, y: 25 }}
+          initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: duration + 0.5 }}
         >
