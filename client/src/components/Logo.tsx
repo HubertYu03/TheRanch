0008c39838ea794fed3c0f655031cca7de0dd651
Hubert Yu <hubyu9234@gmail.com>
2025-05-26 15:19:29 -0700

    Push from the laptop

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <div
-      className="absolute top-5 left-1/2 transform -translate-x-1/2
+      className="absolute top-5 left-5
       flex flex-col items-center text-center"
     >
       <div className="text-4xl sm:text-5xl">The Ranch</div>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import Logo from "@/components/Logo";
+
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
+      <Logo />
       <div className="font-buenard">Home Page</div>
     </div>
   );
